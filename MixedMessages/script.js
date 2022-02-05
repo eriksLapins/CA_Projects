f3c791a9a4c83577797fb806bd762c16b34a4837
Ēriks Lapiņš <lapinseriks98@gmail.com>
2022-02-05 17:16:02 +0200

    made it more difficult, but more option to be scalable

--- MixedMessages/script.js
+++ MixedMessages/script.js
@@ -1,17 +1,30 @@
-// Hello
-const random1 = ['Friend', 'Enemy', 'Traitor', 'Sock', 'Person', 'Robot'];
-// You are
-const random2 = ['doomed', 'Evil', 'my best friend', 'a threat to humanity', 'not dead'];
-// Thank you
-const random3 = ['bye', 'die', 'cry', 'I\'ll go cry', 'just let me breathe', 'I don\'t like you']
 
-const mixedMessages = (array1, array2, array3) => {
-    let random1Num = Math.floor(Math.random()*array1.length);
-    let random2Num = Math.floor(Math.random()*array2.length);
-    let random3Num = Math.floor(Math.random()*array3.length);
+// Set up of message parts
 
-    return `Hello ${array1[random1Num]}. You are ${array2[random2Num]}. Thank you, ${array3[random3Num]}!`;
+const greeterMessage = {
+    greeter: ['Hello,', 'Good morning,', 'Good Evening,', 'Shalom,', 'Ciao,'],
+    identificator: ['Friend.', 'Enemy.', 'Traitor.', 'Sock.', 'Person.', 'Robot.'],
+    youAre: ['You are'],
+    identityName: ['doomed.', 'Evil.', 'my best friend.', 'a threat to humanity.', 'not dead.'],
+    thankYou: ['Thank you,'],
+    messager: ['bye', 'die', 'cry', 'I\'ll go cry', 'just let me breathe', 'I don\'t like you']
+}
+
+function randomNumber(number) {
+    return Math.floor(Math.random() * number);
+}
+
+const mixedMessages = (objectOfArrays) => {
+    let message = [];
+    for (let array in objectOfArrays) {
+        let currentArray = objectOfArrays[array];
+        let randNumber = randomNumber(currentArray.length);
+        message.push(currentArray[randNumber]);
+    }
+    formattedMessage = message.join(' ');
+
+    return formattedMessage;
 };
 
-
-console.log(mixedMessages(random1, random2, random3));
+//mixedMessages(greeterMessage)
+console.log(mixedMessages(greeterMessage));
